4210aa2b6174df4e49bb9200c0a1df82414940c7
ghostbb88 <ghostbb88@gmail.com>
2009-03-06 05:01:35 +0000

    初始化导入工程

---
+++ corner2/branches/corner2-demo/src/test/java/corner/demo/model/one2many/BTest.java
@@ -0,0 +1,44 @@
+//==============================================================================
+//file :        BTest.java
+//
+package corner.demo.model.one2many;
+
+import org.testng.annotations.Test;
+
+import corner.demo.model.BaseModelTestCase;
+
+/**
+ * @author <a href=mailto:ghostbb88@gmail.com>Ghostbb</a>
+ * @version $Revision$
+ * @since 0.0.1
+ */
+public class BTest extends BaseModelTestCase {
+
+	/**
+	 * one2many中B实体的单元测试，这里只测试了保存方法
+	 */
+	@Test(groups="models")
+	public void testB(){
+		A a = new A();
+		a.setLoginName("Ghost");
+		this.saveOrUpdate(a);
+		
+		B b = new B();
+		b.setLoginName("Ghostbb");
+		b.setA(a);
+		this.saveOrUpdate(b);
+		
+		this.flushAndClearSession();
+		
+		String bId = b.getId();
+		
+		b = null;
+		a = null;
+		
+		b = this.load(B.class, bId);
+		
+		assertEquals(b.getLoginName(), "Ghostbb");
+		assertEquals(b.getA().getLoginName(), "Ghost");
+		
+	}
+}
